a7e72dccd03e416caa2a2a0fca152981131aad6e
Bkate12 <49375166+Bkate12@users.noreply.github.com>
2019-04-14 12:09:56 -0600

    Update Assignment.R

--- Assignment.R
+++ Assignment.R
@@ -1,3 +1,10 @@
-my.age <- 24
-my.name <- "Brenna" 
-MakeIntroduction <- Hello, my name is (my.name) and I'm (my.age) years old. 
+my.age <- 24 #Beginning Part 1, make age variable
+my.name <- 'Brenna' #make name variable
+MakeIntroduction <- ("Hello, my name is my.name and I'm my.age years old") #define function MakeIntroduction
+books <- c("The Five People You Meet in Heaven",  "Becoming", "Illusions The Tale of a Reluctant Messiah", "Ishmael", "A Single Man", "Atonement") #Beginning Part 2
+books
+top.three.books <- books[c(1,2,3)] #create variable for top three books in list
+book.reviews <- for (books in books) {   #add "is a great read!" to each book and create new variable 
+books.without.four <- books[c(-4)] #create variable with books without number 4 
+books.without.four
+  long.titles #only books with 15 characters or less
